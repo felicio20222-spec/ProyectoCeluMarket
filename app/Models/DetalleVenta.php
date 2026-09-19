a5d4385aec208bb1337552f4e6c3e725474fe4d7
felicio20222-spec <felicio20222@gmail.com>
2026-09-19 18:37:36 -0300

    Actualización del proyecto Laravel

--- app/Models/DetalleVenta.php
+++ app/Models/DetalleVenta.php
@@ -3,8 +3,25 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Venta;
+use App\Models\Producto;
 
 class DetalleVenta extends Model
 {
-    //
+    protected $fillable = [
+        'venta_id',
+        'producto_id',
+        'cantidad',
+        'precio',
+    ];
+
+    public function venta()
+    {
+        return $this->belongsTo(Venta::class);
+    }
+
+    public function producto()
+    {
+        return $this->belongsTo(Producto::class);
+    }
 }
--- app/Models/Venta.php
+++ app/Models/Venta.php
@@ -3,8 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\DetalleVenta;
 
 class Venta extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'total',
+        'direccion',
+        'ciudad',
+    ];
+
+    public function detalles()
+    {
+        return $this->hasMany(DetalleVenta::class);
+    }
 }
